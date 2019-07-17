4cbce2817573fe19ca4aae13432b18e360dbd89c
Alan Nguyen <tuan8101@gmail.com>
2019-07-17 12:09:59 -0600

    Fix indent

--- 00_binary_search.py
+++ 00_binary_search.py
@@ -8,21 +8,25 @@ def binary_search(list, item):
         # check the middle element
         mid = (low + high) // 2
         guess = list[mid]
+
         # found the item
         if guess == item:
             return mid
-            # the guess was too high
+
+        # the guess was too high
         if guess > item:
             high = mid - 1
-                # the guess was too low
+        # the guess was too low
         else:
             low = mid + 1
 
     # item doesn't exit
     return None
 
-my_list = [1, 3 ,5, 7, 9]
 
-print(binary_search(my_list, 3)) # => 1
+my_list = [1, 3, 5, 7, 9]
 
-print(binary_search(my_list, -1)) # => None
+print(binary_search(my_list, 3))
+
+print(binary_search(my_list, -1))
+
