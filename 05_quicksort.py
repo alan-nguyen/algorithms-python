e83966641934e8bd64388cef8754009a9dc4a279
Alan Nguyen <tuan8101@gmail.com>
2019-07-20 00:02:12 -0600

    Create quick sort function using recursive

--- 05_quicksort.py
+++ 05_quicksort.py
@@ -3,3 +3,14 @@ def quicksot(array):
     # base case, array with 0 or 1 element are already sorted
     if len(arrar) < 2:
         return array
+    # recursive case
+    else:
+        pivot = array[0]
+        # sub-array of all the elements less than the pivot
+        less = [i for i in array[1:] if i <= pivot]
+        # sub-array of all the elements greater than the pivot
+        greater = [i for i in array[1:] if i > pivot]
+        return quicksot(less) + [pivot] + quicksot(greater)
+
+
+
